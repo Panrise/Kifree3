b4b4bce563f34cf400c569b18eb03c5b38e8d364
Panrise <70122578+Panrise@users.noreply.github.com>
2020-12-09 12:04:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ $(document).on("click", "#sendButton", function () {
 		var object = JSON.parse(string);
 		var myHTML = '';
 		for (var i = 0; i < object.length; i++) {
-			myHTML += '<div class="balloon6">';
-			myHTML += '  <div class="name">';
-			myHTML += object[i].name;
-			myHTML += '  </div>';
-			myHTML += '  <div class="chatting">';
-			myHTML += '    <div class="says">';
-			myHTML += object[i].message;
-			myHTML += '    </div>';
-			myHTML += '  </div>';
-			myHTML += '</div>';
-			myHTML  += object[i].room;
+			if (object[i].room=="福岡大学") {
+
 			
 
+				myHTML += '<div class="balloon6">';
+				myHTML += '  <div class="name">';
+				myHTML += object[i].name;
+				myHTML += '  </div>';
+				myHTML += '  <div class="chatting">';
+				myHTML += '    <div class="says">';
+				myHTML += object[i].message;
+				myHTML += '    </div>';
+				myHTML += '  </div>';
+				myHTML += '</div>';
+				myHTML += object[i].room;
+			}
 		}
 		$("#message").html(myHTML);
 	});
