22019d50cfa4a4f4893f5929cfb3e4d1257ff4ea
Panrise <70122578+Panrise@users.noreply.github.com>
2020-12-09 11:35:50 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 $(document).on("click", "#sendButton", function () {
 	var sendName = $("#sendName").val();
 	var sendText = $("#sendText").val();
+	var sendroom = $("#sendroom").val();
 	var url = "https://taketake.azurewebsites.net/api/HttpTrigger1?code=WRYArh2Z8sXoJM0VyKf0hgzk6UWhXQr2sZbCZTB9LtiBLKZbA/BETg==";
-	url = url + "&name=" + sendName + "&message=" + sendText;
+	url = url + "&name=" + sendName + "&message=" + sendText+ "&room" + sendroom ;
 	$.get(url).done(function (string) {
 		var object = JSON.parse(string);
 		var myHTML = '';
